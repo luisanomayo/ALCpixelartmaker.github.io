b2a7c9b323896613ae2430955d5f26c4df61927f
Amen Avan-Nomayo <36841345+luisanomayo@users.noreply.github.com>
2018-06-30 11:31:56 +0200

    Upates
    Inclusion of comments

--- designs.js
+++ designs.js
@@ -4,6 +4,7 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid(width, height) {
+  //makeGrid function declaration to build the design canvas with dimensions width x height
   for (let r = 1; width >= r; r++) {
     $("table").append("<tr></tr>");
     for (let c = 1; height >= c; c++) {
@@ -20,18 +21,23 @@ let height, width, color;
 
 $(document).ready(function() {
   $("#sizePicker").submit(function(event) {
-    $("table tr").remove();
+    $("table tr").remove()//clear any grid on the screen;
     event.preventDefault();
     const height = $("#inputHeight").val();
     const width = $("#inputWeight").val();
-    color = $("#colorPicker").val();
+    let color = $("#colorPicker").val();
     $('#colorPicker').change(function(){
+      //track and effect changes to the color bar
       color = $(this).val()
     })
     makeGrid(height, width);
+  
     $(".colorClass").click(function(event) {
+      //inclusion of color in any grid cell clicked on by user
       $(event.target).css("background-color", color);
+      
       $('.colorClass').dblclick(function(event){
+        //removal of color from a clicked cell already colored
         $(event.target).css('background-color','');
       })
     });
